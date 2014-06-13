e455e7730e0970efb52ef4aa9b6280c2562eeb1d
Kacper Kula <kacper.kula@gmail.com>
2014-06-13 20:01:27 +0200

    Fixed, first working version.

--- src/android/OpenApplication.java
+++ src/android/OpenApplication.java
@@ -4,21 +4,25 @@ import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.CallbackContext;
 import android.content.Intent;
 import android.content.Context;
+import android.content.pm.PackageManager;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+import android.util.Log;
 
 
-public class Intent extends CordovaPlugin {
+public class OpenApplication extends CordovaPlugin {
     
     @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
+        Log.d("kulak", "EXECUTING");
         Context context = this.cordova.getActivity().getApplicationContext();
         Intent i = new Intent(Intent.ACTION_MAIN);
-        PackageManager manager = getPackageManager();
+        PackageManager manager = context.getPackageManager();
         i = manager.getLaunchIntentForPackage(action);
         i.addCategory(Intent.CATEGORY_LAUNCHER);
         context.startActivity(i);
+        return true;
     }
 }
